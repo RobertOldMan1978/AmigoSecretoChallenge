c06d59def351ab08e54d3a936cd61c76a1fca119
OldMan <thingol25@gmail.com>
2025-01-27 22:38:57 -0300

    Actual 2701 2300

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// crea lista
+let listaDeNombres[];
+
+function agregarAmigo(texto) {
+    let imput = document.getElementById("Imput Nombre");
+    let texto = imput.value.trim();
+
+    if (texto === "" ) {
+        else 
+    }
+}
+
